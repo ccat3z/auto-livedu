1cfb6a0664417f40de352086162b4b8d687f50f2
ccat3z <fzhang.chn@foxmail.com>
2021-09-27 16:30:48 +0800

    implement auto play

--- src/common.ts
+++ src/common.ts
@@ -35,3 +35,68 @@ export async function getElement<TElement extends Element> (selector: string) {
 export function exportGlobal(name: string, o: any) {
   (unsafeWindow as any)[name] = o
 }
+
+/**
+ * A utility function for userscripts that detects and handles AJAXed content.
+ *
+ * @author CoeJoder
+ * @link https://github.com/CoeJoder/waitForKeyElements.js
+ * @example
+ * waitForKeyElements("div.comments", (element) => {
+ *   element.innerHTML = "This text inserted by waitForKeyElements().";
+ * });
+ *
+ * waitForKeyElements(() => {
+ *   const iframe = document.querySelector('iframe');
+ *   if (iframe) {
+ *     const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+ *     return iframeDoc.querySelectorAll("div.comments");
+ *   }
+ *   return null;
+ * }, callbackFunc);
+ *
+ * @param {(string|function)} selectorOrFunction - The selector string or function.
+ * @param {function}          callback           - The callback function; takes a single DOM element as parameter.
+ *                                                 If returns true, element will be processed again on subsequent iterations.
+ * @param {boolean}           [waitOnce=true]    - Whether to stop after the first elements are found.
+ * @param {number}            [interval=300]     - The time (ms) to wait between iterations.
+ * @param {number}            [maxIntervals=-1]  - The max number of intervals to run (negative number for unlimited).
+ */
+export function waitForKeyElements<T extends Element>(selectorOrFunction: string | (() => ArrayLike<T>), callback: (elem: T) => any, waitOnce: boolean = true, interval: number = 300, maxIntervals: number = -1) {
+    if (typeof waitOnce === "undefined") {
+        waitOnce = true
+    }
+    if (typeof interval === "undefined") {
+        interval = 300
+    }
+    if (typeof maxIntervals === "undefined") {
+        maxIntervals = -1
+    }
+    var targetNodes = (typeof selectorOrFunction === "function")
+            ? selectorOrFunction()
+            : document.querySelectorAll<T>(selectorOrFunction)
+
+    var targetsFound = targetNodes && targetNodes.length > 0
+    if (targetsFound) {
+        Array.from(targetNodes).forEach(function(targetNode) {
+            var attrAlreadyFound = "data-userscript-alreadyFound"
+            var alreadyFound = targetNode.getAttribute(attrAlreadyFound) || false
+            if (!alreadyFound) {
+                var cancelFound = callback(targetNode)
+                if (cancelFound) {
+                    targetsFound = false
+                }
+                else {
+                    targetNode.setAttribute(attrAlreadyFound, 'true')
+                }
+            }
+        })
+    }
+
+    if (maxIntervals !== 0 && !(targetsFound && waitOnce)) {
+        maxIntervals -= 1
+        setTimeout(function() {
+            waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals)
+        }, interval)
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,52 @@
-import { exportGlobal } from './common'
+import { exportGlobal, waitForKeyElements } from './common'
 import { findAllChapters, findVideosOfChapter, getPlayingChapter, gotoNextChapter } from './utils'
+
+const playVideo = (el: HTMLVideoElement): void => {
+  el.muted = true  // See: https://developer.mozilla.org/en-US/docs/Web/Media/Autoplay_guide#autoplay_availability
+  el.parentElement.querySelector<HTMLAnchorElement>('div.video-img > a').click()
+}
 
 for (let [name, obj] of Object.entries({
   findAllChapters,
   findVideosOfChapter,
   getPlayingChapter,
-  gotoNextChapter
+  gotoNextChapter,
+  playVideo
 })) {
   exportGlobal(name, obj)
 }
+
+waitForKeyElements(
+  () => document.querySelectorAll<HTMLIFrameElement>('iframe#zwshow'),
+  (iframe) => {
+    iframe.addEventListener('load', () => {
+      let videos = Array.from(iframe.contentDocument.querySelectorAll<HTMLVideoElement>('video.myVideo'))
+
+      // Goto next chapter if no video
+      if (videos.length === 0) {
+        console.log('No video found, goto next chapter')
+        gotoNextChapter()
+      }
+
+      for (let [i, video] of videos.entries()) {
+        video.addEventListener('ended', () => {
+          setTimeout(() => {
+            const nextVideo = videos[i + 1]
+
+            if (nextVideo) {
+              console.log('Play next video')
+              playVideo(nextVideo)
+            } else {
+              // No video left
+              console.log('Goto next chapter')
+              gotoNextChapter()
+            }
+          })
+        })
+      }
+
+      // Auto plat first video
+      playVideo(videos[0])
+    })
+  }
+)
